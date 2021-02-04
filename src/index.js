582138d8ca38858c3b24b4473881290fdb509e6f
Alexander0152 <66972872+Alexander0152@users.noreply.github.com>
2021-02-04 18:41:54 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    var result;
+    if (n < 0) {
+        result = parseInt(String(-1*n).split("").reverse().join(""));
+    }
+    else if (n >= 0) {
+        result = parseInt(String(n).split("").reverse().join(""));
+    }
+    else result = undefined;
+
+    return result;
 }
